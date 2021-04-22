a8a98a53be8efccd5b6e6d0829150e5039cffb39
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-04-22 13:36:11 +0530

    Create myownsum.cpp

---
+++ myownsum.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void sum(int arr[],int sum){
+
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bool t[n+1][sum+1];
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0) t[0][j]==false;
+            else if(j==0) t[i][0]== true;
+        }
+
+
+    }
+    cout<<t;
+
+
+}
+
+int main(){
+
+    int set[] = { 3, 34, 4, 12, 5, 2 };
+	int sum = 9;
+    sum(set,sum);
+
+    return 0;
+    
+}
